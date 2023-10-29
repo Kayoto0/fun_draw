a45ed08d421e90048882856580ee3d2a137c3cef
Kayoto0 <148847991+Kayoto0@users.noreply.github.com>
2023-10-29 17:11:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -54,37 +54,38 @@ const drawTriangle = (e) => {
     ctx.closePath(); // closing path of a triangle so the third line draw automatically
     fillColor.checked ? ctx.fill() : ctx.stroke(); // if fillColor is checked fill triangle else draw border
 }
-
 const startDraw = (e) => {
     isDrawing = true;
-    prevMouseX = e.offsetX; // passing current mouseX position as prevMouseX value
-    prevMouseY = e.offsetY; // passing current mouseY position as prevMouseY value
-    ctx.beginPath(); // creating new path to draw
-    ctx.lineWidth = brushWidth; // passing brushSize as line width
-    ctx.strokeStyle = selectedColor; // passing selectedColor as stroke style
-    ctx.fillStyle = selectedColor; // passing selectedColor as fill style
-    // copying canvas data & passing as snapshot value.. this avoids dragging the image
+    const touch = e.touches[0]; // Get the first touch event
+    prevMouseX = touch.clientX - canvas.getBoundingClientRect().left;
+    prevMouseY = touch.clientY - canvas.getBoundingClientRect().top;
+    ctx.beginPath();
+    ctx.lineWidth = brushWidth;
+    ctx.strokeStyle = selectedColor;
+    ctx.fillStyle = selectedColor;
     snapshot = ctx.getImageData(0, 0, canvas.width, canvas.height);
 }
-
 const drawing = (e) => {
-    if(!isDrawing) return; // if isDrawing is false return from here
-    ctx.putImageData(snapshot, 0, 0); // adding copied canvas data on to this canvas
+    if (!isDrawing) return;
+    ctx.putImageData(snapshot, 0, 0);
 
-    if(selectedTool === "brush" || selectedTool === "eraser") {
-        // if selected tool is eraser then set strokeStyle to white 
-        // to paint white color on to the existing canvas content else set the stroke color to selected color
+    const touch = e.touches[0]; // Get the first touch event
+    const offsetX = touch.clientX - canvas.getBoundingClientRect().left;
+    const offsetY = touch.clientY - canvas.getBoundingClientRect().top;
+
+    if (selectedTool === "brush" || selectedTool === "eraser") {
         ctx.strokeStyle = selectedTool === "eraser" ? "#fff" : selectedColor;
-        ctx.lineTo(e.offsetX, e.offsetY); // creating line according to the mouse pointer
-        ctx.stroke(); // drawing/filling line with color
-    } else if(selectedTool === "rectangle"){
-        drawRect(e);
-    } else if(selectedTool === "circle"){
-        drawCircle(e);
+        ctx.lineTo(offsetX, offsetY);
+        ctx.stroke();
+    } else if (selectedTool === "rectangle") {
+        drawRect({ offsetX, offsetY });
+    } else if (selectedTool === "circle") {
+        drawCircle({ offsetX, offsetY });
     } else {
-        drawTriangle(e);
+        drawTriangle({ offsetX, offsetY });
     }
 }
+
 
 toolBtns.forEach(btn => {
     btn.addEventListener("click", () => { // adding click event to all tool option
@@ -125,6 +126,6 @@ saveImg.addEventListener("click", () => {
     link.click(); // clicking link to download image
 });
 
-canvas.addEventListener("mousedown", startDraw);
-canvas.addEventListener("mousemove", drawing);
-canvas.addEventListener("mouseup", () => isDrawing = false);
+canvas.addEventListener("touchstart", startDraw);
+canvas.addEventListener("touchmove", drawing);
+canvas.addEventListener("touchend", () => isDrawing = false);
